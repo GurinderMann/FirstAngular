f1107678e27830d31dfb3b3d01269d233cb2c3b0
GurinderMann <manngurinder110@gmail.com>
2023-07-24 14:46:04 +0200

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
